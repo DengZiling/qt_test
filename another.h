052425eab9f827baa4e36ff69e78cac8f3969b01
DengZiling <843076712@qq.com>
2021-05-22 22:56:43 +0800

    first commit

---
+++ another.h
@@ -0,0 +1,15 @@
+#ifndef ANOTHER_H
+#define ANOTHER_H
+
+#include <string.h>
+#include <stdlib.h>
+#include <iostream>
+class myClass{
+private:
+    std::string str1;
+public:
+    void write(std::string str);
+    void read(std::string str);
+}
+
+#endif // ANOTHER_H
